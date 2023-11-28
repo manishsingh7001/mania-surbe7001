a54a9b82a13258d2f839baa4e0edade680d1a681
hemraj <hemrajsaini8078@gmail.com>
2023-11-28 14:21:51 +0530

    get code

--- Backend/db/dbconfig.js
+++ Backend/db/dbconfig.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/e-commerce").then( ()=> {
+mongoose.connect("mongodb+srv://hemrajsaini8078:aNCeSvdEFE5yKVNK@cluster0.klilxg0.mongodb.net/e-commerce?retryWrites=true&w=majority").then( ()=> {
     console.log(`MongoDB Connected Successfully`);
 })
+
+
+// e-commerce
