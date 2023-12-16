c5b9cc42623a9fa335f63954651256b7b3e20dff
hemraj <hemrajsaini8078@gmail.com>
2023-12-16 13:50:27 +0530

    Token update

--- Frontend/regex/src/Admin/componentpage/quizz/QuizList.js
+++ Frontend/regex/src/Admin/componentpage/quizz/QuizList.js
@@ -1,23 +1,32 @@
 // QuizList.js
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { navigate } from "react-router-dom";
 
 const QuizList = () => {
   const [quizzes, setQuizzes] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchQuizzes = async () => {
       try {
-        const response = await fetch('http://localhost:5000/quizzes');
+        const response = await fetch("http://localhost:5000/quizzes");
         const data = await response.json();
         setQuizzes(data);
       } catch (error) {
-        console.error('Error fetching quizzes:', error);
+        console.error("Error fetching quizzes:", error);
       }
     };
 
     fetchQuizzes();
   }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("adminToken");
+    if (!token) {
+      console.log("Token not found. Navigating to login.");
+      navigate("/admin/login");
+    }
+  });
 
   return (
     <div>
